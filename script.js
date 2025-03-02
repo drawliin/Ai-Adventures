86dd1429dcfff27a397c36d327fe4d8f98a25064
drawlin <itshoussameddine@gmail.com>
2025-03-02 11:15:58 +0000

    handling collision

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ function gameLoop() {
 
   // Update and draw game objects here
   dataBot.draw(ctx);
+  dataPoints.forEach(point => point.draw(ctx));
+  dataBot.collectData(dataPoints); // Check for collisions
 
   requestAnimationFrame(gameLoop);
 }
 
+// DATABOT CLASS
 class DataBot {
     constructor(x, y, size, color, speed) {
       this.x = x;
@@ -34,10 +37,40 @@ class DataBot {
       this.x += dx * this.speed;
       this.y += dy * this.speed;
     }
+
+    collectData(dataPoints) {
+      dataPoints.forEach((point, index) => {
+        if (
+          this.x < point.x + point.size &&
+          this.x + this.size > point.x &&
+          this.y < point.y + point.size &&
+          this.y + this.size > point.y
+        ) {
+          dataPoints.splice(index, 1); // Remove the collected data point
+          console.log("Data collected!");
+        }
+      });
+    }
 }
 
+// DATAPOINT CLASS
+class DataPoint {
+    constructor(x, y, size, color) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.color = color;
+    }
+  
+    draw(ctx) {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+  }
+// DATABOT OBJECT
 const dataBot = new DataBot(250, 250, 20, "red", 5);
 
+// MOVING DATABOT
 document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowUp") dataBot.move(0, -1);
     if (e.key === "ArrowDown") dataBot.move(0, 1);
@@ -45,6 +78,13 @@ document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight") dataBot.move(1, 0);
   });
 
+
+// CREATING DATAPOINTS OBJECTS
+const dataPoints = [
+  new DataPoint(300, 300, 10, "green"),
+  new DataPoint(500, 200, 10, "green"),
+  new DataPoint(100, 400, 10, "green")
+];
 
 
 // Start the game loop
